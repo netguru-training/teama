9f192ee185420691505cf0664cde24d923e34a4c
mkarbowiak <trnks01@gmail.com>
2015-05-23 12:46:30 +0200

    expose board_game

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,4 +1,8 @@
 class BoardGamesController < ApplicationController
+  expose(:board_game)
 
-
+  private
+  def board_game_params
+    params.require(:board_game).permit(:name, :description)
+  end
 end
