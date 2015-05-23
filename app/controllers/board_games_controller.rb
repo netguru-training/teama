02e72aa0f8ea044b0a99d2662c37f8691abbff7e
mkarbowiak <trnks01@gmail.com>
2015-05-23 12:49:45 +0200

    move method from private

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,14 +1,14 @@
 class BoardGamesController < ApplicationController
   expose(:board_game)
 
-  private
-  def board_game_params
-    params.require(:board_game).permit(:name, :description)
-  end
-
   def add_to_collection
     bg = BoardGame.find(params[:id])
     current_user.collections.create(board_game: bg)
     redirect_to :back, notice: 'Game added to user.'
+  end
+
+  private
+  def board_game_params
+    params.require(:board_game).permit(:name, :description)
   end
 end
