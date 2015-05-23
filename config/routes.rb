66d90d00cf6e2bd4d5d50a19f05988e71e9c036c
mkarbowiak <trnks01@gmail.com>
2015-05-23 11:54:01 +0200

    change users/show to users/profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
-
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   get "/pages/home" => 'pages#home', as: :home
+  get '/users/profile', to: 'users#profile', as: :profile
   root to: 'pages#home'
 end
