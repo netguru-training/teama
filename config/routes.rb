8cb5a93d159af2572354732b2b1e824e823498fd
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-05-23 11:58:28 +0200

    [WIP] add board_game_to_collection

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   get "/pages/home" => 'pages#home', as: :home
-  post '/board_game/assign_game_to_user' => 'board_game#assign_game_to_user', as: :assign_game_to_user
+  # post '/board_game/assign_game_to_user' => 'board_game#assign_game_to_user', as: :assign_game_to_user
 
 end
   root to: 'pages#home'
