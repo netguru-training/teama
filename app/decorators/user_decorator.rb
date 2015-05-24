d66660d2bfc26c437ade610027947a16de78c7d9
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-05-24 13:35:13 +0200

    added average rating to user

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,4 +1,12 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
+  def average_rating
+  	if object.reviews.present?
+			object.reviews.pluck(:rating).reduce(:+)
+		else
+			"-"
+		end
+  end
+
 end
