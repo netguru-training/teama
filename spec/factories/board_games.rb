b53191c690ea74fa1f8d0919c9195ae109d84848
mkarbowiak <trnks01@gmail.com>
2015-05-24 14:42:16 +0200

    update board_game model tests

--- spec/factories/board_games.rb
+++ spec/factories/board_games.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :board_game do
     name Faker::Lorem.sentence
     description Faker::Lorem.paragraph
-    average_rating 1.5
   end
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
 describe Comment do
-  let(:comment) { create(:comment) }
-
   it { should validate_presence_of(:content) }
 end
