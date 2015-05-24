2eaf728efeba60c8f868f770e364dd3b603c08e5
mkarbowiak <trnks01@gmail.com>
2015-05-24 12:26:02 +0200

    style homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   expose(:best_games) { BoardGame.highest_rated }
   expose(:board_games)
   expose(:users)
+  expose(:latest_reviews) { Review.last(5) }
 
   def home
   end
