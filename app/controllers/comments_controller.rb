a169426679bbe00dd8848ee68b090df8d712d2c9
mkarbowiak <trnks01@gmail.com>
2015-05-24 16:09:18 +0200

    prevent guests from writing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   expose(:comment, attributes: :comment_params)
   expose(:review)
   # expose(:board_game)
