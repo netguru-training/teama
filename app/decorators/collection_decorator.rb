76ba522dc3fee723d21912a9fc1853dec23b58d9
mkarbowiak <trnks01@gmail.com>
2015-05-24 09:55:52 +0200

    add collection decorator

--- app/decorators/collection_decorator.rb
+++ app/decorators/collection_decorator.rb
@@ -5,4 +5,13 @@ class BoardGameDecorator < Draper::Decorator
     h.link_to('Edit', edit_board_game_path(object)) if object.collections.any? { |col| col.user_id == current_user.id }
   end
 
+  def add_link(current_user, game)
+    h.link_to('Add', board_game_collections_path(game)) if board_games_ids.include?(game.id)
+  end
+
+  private
+
+  def board_games_ids
+    @ids ||= current_user.collections.map(&:board_game_id)
+  end
 end
