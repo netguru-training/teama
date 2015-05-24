a76fb1645af73348d32e0b9e044b6a49f18a5648
mkarbowiak <trnks01@gmail.com>
2015-05-24 14:24:10 +0200

    add board game tests and validations

--- spec/models/board_game_spec.rb
+++ spec/models/board_game_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 describe BoardGame do
-  let(:board_game) { build(:board_game) }
+  let(:board_game) { create(:board_game) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
 end
