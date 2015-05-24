8d2fff3a2078d1d4bee104e4aed14a01c15b94fa
mkarbowiak <trnks01@gmail.com>
2015-05-24 13:16:07 +0200

    add database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,11 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
