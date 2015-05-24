438c50b702c0484aa04bb57a40777eff484d83d9
mkarbowiak <trnks01@gmail.com>
2015-05-24 10:27:09 +0200

    hide add button for already added games

--- app/decorators/board_game_decorator.rb
+++ app/decorators/board_game_decorator.rb
@@ -1,4 +1,13 @@
 class BoardGameDecorator < Draper::Decorator
   delegate_all
 
+  def add_link(game, current_user)
+    h.button_to('Add', h.board_game_collections_path(game), method: :post) unless board_games_ids(current_user).include?(game.id)
+  end
+
+  private
+
+  def board_games_ids(current_user)
+    @ids ||= current_user.collections.map(&:board_game_id)
+  end
 end
